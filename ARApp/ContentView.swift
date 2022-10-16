5c42ebaa3d7731a93e283fac82af4961d816ec0e
Mridang Sheth <mridangs@gmail.com>
2022-10-16 00:54:15 -0700

    Resolved conflicts

--- ARApp/ContentView.swift
+++ ARApp/ContentView.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
-
+    @State var supportsLIDAR: Bool = false
+    @State var sceneDepthStr: String
     var body: some View {
         ZStack {
-            ARViewContainer().edgesIgnoringSafeArea(.all)
+            ARViewContainer(supportsLIDAR: $supportsLIDAR, sceneDepthStr: $sceneDepthStr).edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Text("Supports LIDAR: \(supportsLIDAR.description)")
+                Text("Depth: \(self.sceneDepthStr)")
+                Spacer()
+            }
             
             Button(
                 action: {
@@ -21,29 +29,84 @@ struct ContentView : View {
             ) {
                 Text("Take a Picture").frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            
         }
-//        Color.clear
-//            .contentShape(Rectangle())
-//            .onTapGesture {
-//                print("I PRESSED.")
-//            }
     }
+    
+    
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    
+    @Binding var supportsLIDAR: Bool
+    @Binding var sceneDepthStr: String
     
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        // Start AR session
+        let session = arView.session
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        if ARWorldTrackingConfiguration.supportsFrameSemantics(.smoothedSceneDepth) {
+            config.frameSemantics = .smoothedSceneDepth
+            
+            
+            DispatchQueue.main.async {
+                supportsLIDAR = true
+            }
+        } else {
+            // TODO: Raise Error
+        }
+        session.delegate = context.coordinator
+        session.run(config)
+
+        // Add coaching overlay
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.session = session
+        coachingOverlay.goal = .horizontalPlane
+        arView.addSubview(coachingOverlay)
+        
+        
         
         return arView
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(sceneDepthStr: $sceneDepthStr)
+    }
+    
+    class Coordinator: NSObject, ARSessionDelegate {
+        @Binding var sceneDepthStr: String
+    
+        init(sceneDepthStr: Binding<String>) {
+            _sceneDepthStr = sceneDepthStr
+        }
+        
+        func session(_ session: ARSession, didUpdate frame: ARFrame) {
+            if let sceneDepth = frame.smoothedSceneDepth {
+                let depthData = sceneDepth.depthMap
+                let depthWidth = CVPixelBufferGetWidth(depthData)
+                let depthHeight = CVPixelBufferGetHeight(depthData)
+                
+                CVPixelBufferLockBaseAddress(depthData, CVPixelBufferLockFlags(rawValue: 0))
+                let floatBuffer = unsafeBitCast(CVPixelBufferGetBaseAddress(depthData), to: UnsafeMutablePointer<Float32>.self)
+                
+                var minDist: Float32 = 1000000
+                for y in 0...depthHeight-1 {
+                    for x in 0...depthWidth-1 {
+                        let distXY = floatBuffer[y * depthWidth + x]
+                        if minDist > distXY {
+                            minDist = distXY
+                        }
+                    }
+                }
+                self.sceneDepthStr = "\(round(minDist * 100) / 100.0)"
+            }
+        }
         
     }
     
@@ -51,10 +114,11 @@ struct ARViewContainer: UIViewRepresentable {
     
 }
 
+
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(sceneDepthStr: "")
     }
 }
 #endif
